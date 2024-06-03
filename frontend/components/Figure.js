f165fa924a4b0447fab6877c336035d827238b3e
Lun Ng <lunngbill@gmail.com>
2024-06-02 21:17:00 -0700

    finished

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure `
+  max-width: 5000px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  figcaption {
+    text-align: center
+    font-style: italic;
+  }
+
+`
 
 export default function Figure({imageURL, caption}) {
     return (
-      <figure>
+      <StyledFigure>
       <img src={imageURL} />
       <figcaption>Awesome pic taken on {caption}</figcaption>
-    </figure>
+    </StyledFigure>
     )
   }
   
